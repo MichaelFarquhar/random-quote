d6580f7763263c4e3d760e564ee56c7c8bf89baf
Michael <michaelfarquhar4@gmail.com>
2022-11-20 11:45:04 -0500

    Improve CSS and generation of new quote with SPACE

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, KeyboardEvent } from 'react';
 import { themeChange } from 'theme-change';
 import { ThemeSelect } from './ThemeSelect/ThemeSelect';
 
@@ -13,6 +13,11 @@ function App() {
         content: '',
         author: '',
     });
+
+    // Generate a new quote when the SPACEBAR is pressed
+    const handleKeyboardEvent = (e: KeyboardEvent<HTMLElement>) => {
+        if (e.key == ' ') getQuote();
+    };
 
     const getQuote = () => {
         axios.get('https://api.quotable.io/random').then((res) =>
@@ -30,8 +35,9 @@ function App() {
     }, []);
 
     return (
-        <div className="App">
+        <div className="App" onKeyDown={handleKeyboardEvent} tabIndex={0}>
             <header className="App-header bg-base-200">
+                <ThemeSelect />
                 <div className="card bg-base-100 shadow-xl w-10/12 lg:w-6/12">
                     <div className="card-body text-center">
                         {quote.content === '' ? (
@@ -44,14 +50,30 @@ function App() {
                                 <p className="italic">- {quote.author}</p>
                             </>
                         )}
-                        <div className="card-actions justify-between items-end mt-12">
-                            <ThemeSelect />
-                            <button
-                                className="btn btn-primary"
-                                onClick={() => getQuote()}
-                            >
-                                New Quote
-                            </button>
+                        <div className="card-actions justify-center mt-10">
+                            <div>
+                                <button
+                                    className="btn btn-primary gap-2"
+                                    onClick={() => getQuote()}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 24 24"
+                                        fill="currentColor"
+                                        className="w-5 h-5"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M12 1.5a.75.75 0 01.75.75V4.5a.75.75 0 01-1.5 0V2.25A.75.75 0 0112 1.5zM5.636 4.136a.75.75 0 011.06 0l1.592 1.591a.75.75 0 01-1.061 1.06l-1.591-1.59a.75.75 0 010-1.061zm12.728 0a.75.75 0 010 1.06l-1.591 1.592a.75.75 0 01-1.06-1.061l1.59-1.591a.75.75 0 011.061 0zm-6.816 4.496a.75.75 0 01.82.311l5.228 7.917a.75.75 0 01-.777 1.148l-2.097-.43 1.045 3.9a.75.75 0 01-1.45.388l-1.044-3.899-1.601 1.42a.75.75 0 01-1.247-.606l.569-9.47a.75.75 0 01.554-.68zM3 10.5a.75.75 0 01.75-.75H6a.75.75 0 010 1.5H3.75A.75.75 0 013 10.5zm14.25 0a.75.75 0 01.75-.75h2.25a.75.75 0 010 1.5H18a.75.75 0 01-.75-.75zm-8.962 3.712a.75.75 0 010 1.061l-1.591 1.591a.75.75 0 11-1.061-1.06l1.591-1.592a.75.75 0 011.06 0z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                    New Random Quote
+                                </button>
+                                <div className="mt-2 text-sm">
+                                    or press <kbd className="kbd kbd-sm">SPACE</kbd>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/ThemeSelect/ThemeSelect.tsx
+++ src/ThemeSelect/ThemeSelect.tsx
@@ -6,18 +6,22 @@ const capitalize = (str: string) => {
 
 export const ThemeSelect = () => {
     return (
-        <div className="form-control max-w-xs">
-            <label className="label">
-                <span className="label-text">Select theme</span>
-            </label>
-            <select
-                className="select select-bordered select-sm max-w-xs"
-                data-choose-theme
-            >
-                {themes.map((theme) => (
-                    <option value={theme}>{capitalize(theme)}</option>
-                ))}
-            </select>
+        <div className="w-10/12 lg:w-6/12 mb-6">
+            <div className="form-control max-w-xs">
+                <label className="label">
+                    <span className="label-text">Change theme</span>
+                </label>
+                <select
+                    className="select select-bordered select-sm max-w-fit"
+                    data-choose-theme
+                >
+                    {themes.map((theme) => (
+                        <option value={theme} key={theme}>
+                            {capitalize(theme)}
+                        </option>
+                    ))}
+                </select>
+            </div>
         </div>
     );
 };
